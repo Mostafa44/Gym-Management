647964ee1c922e0885c02c61f9ed8874698952b1
Mostafa AbdEl-Shafy <mostafa.abdel-shafy@siemens.com>
2025-01-21 12:48:56 +0200

    initial introduction of Repository and UnitOfWork

--- GymManagement.Application/Subscriptions/Commands/CreateSubscriptions/CreateSubscriptionCommand.cs
+++ GymManagement.Application/Subscriptions/Commands/CreateSubscriptions/CreateSubscriptionCommand.cs
@@ -1,11 +1,12 @@
 ﻿using ErrorOr;
+using GymManagement.Domain.Subscriptions;
 using MediatR;
 
 namespace GymManagement.Application.Subscriptions.Commands.CreateSubscriptions
 {
     public record CreateSubscriptionCommand(
         string SubscriptionType,
-        Guid AdminId) : IRequest<ErrorOr<Guid>>;
+        Guid AdminId) : IRequest<ErrorOr<Subscription>>;
 
 
 }
---
+++ GymManagement.Domain/Subscriptions/Subscription.cs
@@ -0,0 +1,7 @@
+namespace GymManagement.Domain.Subscriptions
+{
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+    }
+}
